8d531bbb5c943dd8887f94a029e6a4b59e0c3010
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-04-01 23:20:26 -0600

    update solution

---
+++ solution.py
@@ -0,0 +1,46 @@
+def sumAcumulative(months):
+    months_ = months.copy()
+    n = len(months)-1
+    for i in range(1,n+1):
+        months_[n-i] += months_[n-i+1]
+    return months_
+
+def posible_k(sumAcumulative):
+    n = len(sumAcumulative)
+    list_posible_k =[]
+    k = int(n/2)
+    medium_plus = n-k #cant de elementos que no son todos necesariamente iguales 
+ 
+    for i in range(0,medium_plus):
+        if sumAcumulative[medium_plus-i] > 0: #ultimo elemento de los iguales
+            list_posible_k.append(k)
+        k+=1    
+    return list_posible_k
+    
+
+def solution(months):
+    n = len(months)
+    solution = sumAcumulative(months) #Array con la suma acumulativa
+    if solution[0] >= 0: # Caso base si la suma de todos los elementos es positiva
+        return n
+    print(solution) 
+    k = int(n/2)
+    #medium_plus = n-k #cant de elementos que no son todos necesariamente iguales 
+    value = months[n-k]
+    list_posible_k = posible_k(solution)
+    
+    for k in list_posible_k:
+        for i in range(0,n-k+1):
+            #pos = n-k-i
+            if i+k<=n and solution[i] - value*(n-(i+k)) <0:
+                break
+            if i == n-k:
+                return k
+            
+            #if i+k<=n and solution[pos] - value*(n-(pos+k)) <0:
+            #    break
+            #elif pos==0:
+            #    return k
+
+months = [-5,-5,-5,-5,21,-5,-5,-5,-5]
+print(solution(months))
--- tester.py
+++ tester.py
@@ -4,6 +4,7 @@
 
  
 import random
+import math
 from itertools import permutations
 
 def generate_array(n): 
@@ -22,38 +23,48 @@ def generate_array(n):
             
     return months
 
-# first solution
-def solution(months):
+def is_correct_solution(k,list_k):
+    if list_k.count(k):
+        return True
+    return False
+    
+# test solution
+def test_solution(months):
     n = len(months)
-   
-    mean = int(n/2)
-    if n%2 !=0: #Si es impar el n\'umero de meses
-        mean+=1
-   
-    k=1
-    if months[mean+1]<0:
-        k = mean
-        
-    index = 0
-    while k<n:
-        
-        result = sum(months[index:index+k])
-        index +=1
+    list_k = []
+    for k in range(1,n):    
+        for i in range(0,n-k):
+            result = sum(months[i:i+k])
+            if result < 0: #si k no es valido
+                break
+            elif i + k >= n-k : #Si llegue al final
+                list_k.append(k)
+                break
+    return list_k  
+
+#Solution recursive de un k primer acercamiento
+def solution_recursive(months,list_k,k):
+    n = len(months)
+    result = math.inf  # max valor positivo    
+    for i in range(0,n-k):
+        result = min(result,sum(months[i:i+k]))
+    if result > 0 :
+        return k
+    return solution_recursive(months,list_k,k-1)        
+    
+#Solution recursive de un k segundo acercamiento
+def solution_recursive(months,i,k):
+    n = len(months)
+    if sum(months[i:i+k])  < 0:
+        return solution_recursive(months,0,k-1)
+    if i + k >= n-k :
+        return k        
+
+
+
        
-        if result < 0: #si k no es valido
-            k+=1
-            index = 0
-
-            
-        elif index + k > n-1 : #Si llegue al final
-            return k
-
-
-
-
-months = [-10, 3, -10, 9, -10, 6, 6, 6, 6]
-
-#months =generate_array(12)
+#months = [-5, -4, -6, -5, 21, -5, -5, -5, -5]
+months =generate_array(12)
 print(months)         
 print(solution(months))        
 
