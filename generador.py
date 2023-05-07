7e1dedc3bff6bad79be18c9b4ecba2367fe4befe
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-05-07 15:55:49 -0600

    organizar

--- generador.py
+++ generador.py
@@ -1,22 +1,4 @@
-
-class Edge:
-    def __init__(self, node1, node2, cost):
-        self.node1 = node1
-        self.node2 = node2
-        self.cost = cost
-
-class Node:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-class Graph:
-    def __init__(self, nodes, edges):
-        self.nodes = nodes
-        self.edges = edges
-
-
-
+from graph import Edge,Node
 
 def backtraking(grafo, edges_to_build, cities_to_build, pos):
 
