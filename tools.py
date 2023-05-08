60ebfe2254dd317d259cf5589382773d6303e0c1
Daroce1012 <danielarodriguezcepero@gmail.com>
2023-05-08 00:14:18 -0600

    update Path_EK

--- tools.py
+++ tools.py
@@ -5,7 +5,8 @@ class Edge:
         self.node1 = node1
         self.node2 = node2
         self.capacity = capacity
-        self.flow = 0 
+        self.flow = 0
+        self.reverse = 0 
 
 class Node:
     def __init__(self, name, value):
@@ -17,7 +18,7 @@ class Graph:
         self.edges = edges
 
 
-def BFS(s,t,adj): # s, t representan indices
+def BFS(s,adj): # s, t representan indices
     queue = []
     path = []
     d = []
@@ -37,3 +38,25 @@ def BFS(s,t,adj): # s, t representan indices
                 queue.append(v)    
         
     return path    
+
+
+def PATH_EK(Gf,s,t):#Red residual, fuente, receptor
+    path = BFS(Gf,s)
+    p=[]
+    
+    if len(path)< 0 :
+        return None
+    
+    node_temp = t
+    while node_temp :
+        parent = path[node_temp]
+        p.append(parent)
+        node_temp=parent
+    
+    return p    
+        
+
+def EDMONDS_KARP(G,s,t):
+    edges  = G.edges
+    
+    
